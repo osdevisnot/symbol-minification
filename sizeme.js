8a5816d424284c5748b7ed6c292c79a61b041293
osdevisnot <osdevisnot@gmail.com>
2020-10-20 12:01:46 -0700

    rename dist files

--- sizeme.js
+++ sizeme.js
@@ -3,7 +3,7 @@
 const path = require('path')
 const gzipSize = require('gzip-size')
 
-for (let file of ['esbuild.js', 'terser.js']) {
+for (let file of ['esbuild.out.js', 'terser.out.js']) {
   console.log(
     `${file} => ${gzipSize.fileSync(path.join(__dirname, 'dist', file))}`
   )
